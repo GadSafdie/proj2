3876274348685eec5af544d727707b73b467b7c8
Hezi Avraham <chezi91@gmail.com>
2019-01-09 12:48:17 +0200

    HEZI

---
+++ Searchable.h
@@ -0,0 +1,26 @@
+//
+// Created by hezi on 1/9/19.
+//
+
+#ifndef PROJ2_SEARCHABLE_H
+#define PROJ2_SEARCHABLE_H
+
+#include "State.h"
+#include <vector>
+
+template <class T>
+class Searchable {
+public:
+
+    virtual State <T> getInitalState() = 0;
+
+    virtual State<T> getGoalState() = 0;
+
+    virtual vector<State<T>>getAllPossibleStates(State<T> s) = 0;
+
+    virtual void InitlizeAllStates() = 0;
+
+};
+
+
+#endif //PROJ2_SEARCHABLE_H
